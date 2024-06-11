641a7d5ac2c018cf686b47a2cb4593fa5e15b1ea
frank-hub <franklinekevo@gmail.com>
2024-06-11 23:20:43 +0300

    gridview over listview

--- lib/screens/cart.dart
+++ lib/screens/cart.dart
@@ -18,239 +18,161 @@ class CartScreen extends StatelessWidget {
           IconButton(
             icon: const Icon(Icons.shopping_cart),
             onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder:
-                  (context)=> CartScreen()
-              ));
-              },
+              Navigator.push(context, MaterialPageRoute(builder: (context) => const CartScreen()));
+            },
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                decoration: BoxDecoration(
-                  color: const Color(0xffEEEEEE),
-                  border: Border.all(width: 0.1, color: Colors.black.withOpacity(0.8)),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.symmetric(vertical: 10),
-                      width: 80,
-                      child: const Text("MODEL"),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.symmetric(vertical: 10),
-                      width: 75,
-                      child: const Text("QUANTITY"),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.symmetric(vertical: 10),
-                      child: Text("UNIT\nPRICE"),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.symmetric(vertical: 10),
-                      child: Text("TOTAL"),
-                    ),
-                  ],
-                ),
-              ),
-              Column(
-                children: cartProvider.items.values.map((item) {
-                  return Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    decoration: BoxDecoration(
-                      border: Border.all(width: 0.1, color: Colors.black.withOpacity(0.8)),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.symmetric(vertical: 10),
-                          width: 80,
-                          child: Text(item.product.p_name ?? ''),
-                        ),
-                        Container(
-                          width: 70,
-                          child: Container(
-                            decoration: BoxDecoration(
-                              border: Border.all(width: 0.8, color: Colors.black.withOpacity(0.8)),
-                            ),
-                            child: Column(
-                              children: [
-                                Row(
-                                  children: [
-                                    Container(
-                                      width: 20,
-                                      height: 30,
-                                      child: TextField(
-                                        readOnly: true,
-                                        controller: TextEditingController(text: item.quantity.toString()),
-                                        keyboardType: TextInputType.number,
-                                        onSubmitted: (value) {
-                                          final newQuantity = int.tryParse(value) ?? 1;
-                                          cartProvider.updateQuantity(item.product, newQuantity);
-                                        },
-                                      ),
-                                    ),
-                                    Container(
-                                      color: Colors.grey,
-                                      child: Column(
-                                        children: [
-                                          IconButton(
-                                            icon: const Icon(Icons.keyboard_arrow_up, size: 15),
-                                            onPressed: () {
-                                              cartProvider.updateQuantity(item.product, item.quantity + 1);
-                                            },
-                                          ),
-                                          IconButton(
-                                            icon: const Icon(Icons.keyboard_arrow_down, size: 15),
-                                            onPressed: () {
-                                              cartProvider.updateQuantity(item.product, item.quantity - 1);
-                                            },
-                                          ),
-                                        ],
-                                      ),
-                                    ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            // Cart Items List
+            Expanded(
+              child: ListView.builder(
+                itemCount: cartProvider.items.length,
+                itemBuilder: (context, index) {
+                  var item = cartProvider.items.values.toList()[index];
+                  return Card(
+                    margin: const EdgeInsets.symmetric(vertical: 10),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          // Product Name
 
-                                    // Container(
-                                    //   width: 30,
-                                    //   height: 30,
-                                    //   color: Colors.red,
-                                    //   child: Center(
-                                    //     child: IconButton(
-                                    //       icon: const Icon(Icons.delete_forever, color: Colors.white, size: 18),
-                                    //       onPressed: () {
-                                    //         cartProvider.removeItem(item.product.id ?? 0);
-                                    //       },
-                                    //     ),
-                                    //   ),
-                                    // ),
-                                  ],
+                          SizedBox(
+                            width: 100,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(item.product.p_name ?? '', style: TextStyle(fontSize: 15)),
+                                Text(
+                                  'Ksh ${double.tryParse(item.product.price ?? '')?.toStringAsFixed(2) ?? '0.00'}',
+                                  style: TextStyle(fontSize: 13,color: Colors.grey.withOpacity(0.5)),
                                 ),
                               ],
                             ),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 10),
-                          child: Text(
-                            double.tryParse(item.product.price ?? '')?.toStringAsFixed(2) ?? '0.00',
+                          // Quantity Selector
+                          Row(
+                            children: [
+                              IconButton(
+                                icon: const Icon(Icons.remove),
+                                onPressed: () {
+                                  cartProvider.updateQuantity(item.product, item.quantity - 1);
+                                },
+                              ),
+                              SizedBox(
+                                width: 13,
+                                child: Center(
+                                  child: Text(item.quantity.toString(), style: TextStyle(fontSize: 16)),
+                                ),
+                              ),
+                              IconButton(
+                                icon: const Icon(Icons.add),
+                                onPressed: () {
+                                  cartProvider.updateQuantity(item.product, item.quantity + 1);
+                                },
+                              ),
+                            ],
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 10),
-                          child: Text(
-                            (double.tryParse(item.product.price ?? '')! * item.quantity).toStringAsFixed(2),
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
-                }).toList(),
-              ),
-              const SizedBox(height: 50),
-              Container(
-                width: double.infinity,
-                padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                decoration: const BoxDecoration(color: Color(0xffEEEEEE)),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(height: 10),
-                    const SizedBox(height: 20),
-                    Container(
-                      width: double.infinity,
-                      height: 45,
-                      color: Colors.white,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          const Text(
-                            "Total:",
+                          // Unit Price
+
+                          // Total Price
+                          Text(
+                            'Ksh ${(double.tryParse(item.product.price ?? '')! * item.quantity).toStringAsFixed(2)}',
                             style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold),
                           ),
-                          Container(
-                            width: 65,
-                            padding: const EdgeInsets.only(right: 10, left: 10),
-                            child: Text(cartProvider.totalAmount.toStringAsFixed(2)),
+                          // Remove Item Button
+                          IconButton(
+                            icon: const Icon(Icons.delete, color: Colors.red),
+                            onPressed: () {
+                              cartProvider.removeItem(item.product.id ?? 0);
+                            },
                           ),
                         ],
                       ),
                     ),
-                    const SizedBox(height: 20),
-                    Container(
-                      height: 1,
-                      width: double.infinity,
-                      color: Colors.red.withOpacity(0.2),
-                    ),
-                    const SizedBox(height: 10),
-                    Container(
-                      color: Colors.grey,
-                      height: 45,
-                      child: Center(
-                        child: RichText(
-                          text: const TextSpan(
-                            children: [
-                              WidgetSpan(
-                                child: Icon(CupertinoIcons.left_chevron, size: 18, color: Colors.white70),
-                              ),
-                              WidgetSpan(
-                                child: SizedBox(width: 10),
-                              ),
-                              TextSpan(
-                                text: 'CONTINUE SHOPPING',
-                                style: TextStyle(fontSize: 18),
-                              ),
+                  );
+                },
+              ),
+            ),
+            // Total and Checkout
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              decoration: BoxDecoration(
+                color: const Color(0xffEEEEEE),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        "Total:",
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        '\$${cartProvider.totalAmount.toStringAsFixed(2)}',
+                        style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.grey,
+                            padding: const EdgeInsets.symmetric(vertical: 15),
+                          ),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Icon(CupertinoIcons.left_chevron, size: 18, color: Colors.white70),
+                              SizedBox(width: 10),
+                              Text('CONTINUE SHOPPING', style: TextStyle(fontSize: 10)),
                             ],
                           ),
                         ),
                       ),
-                    ),
-                    const SizedBox(height: 10),
-                    InkWell(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => ShippingAddress()),
-                        );
-                      },
-                      child: Container(
-                        color: Colors.green,
-                        height: 45,
-                        child: Center(
-                          child: RichText(
-                            text: const TextSpan(
-                              children: [
-                                WidgetSpan(
-                                  child: Icon(Icons.exit_to_app, size: 18, color: Colors.white70),
-                                ),
-                                WidgetSpan(
-                                  child: SizedBox(width: 10),
-                                ),
-                                TextSpan(
-                                  text: 'CHECKOUT',
-                                  style: TextStyle(fontSize: 18),
-                                ),
-                              ],
-                            ),
+                      const SizedBox(width: 10),
+                      Expanded(
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.green,
+                            padding: const EdgeInsets.symmetric(vertical: 15),
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => ShippingAddress()),
+                            );
+                          },
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Icon(Icons.exit_to_app, size: 18, color: Colors.white70),
+                              SizedBox(width: 10),
+                              Text('CHECKOUT', style: TextStyle(fontSize: 10)),
+                            ],
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
+                    ],
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
